122e655f503a0eb3edf141095444a87ec7c41318
hasin01 <mrsemenav5@gmail.com>
2023-06-11 19:36:33 +0300

    full

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { ContactPage, LoginPage, MainLayout, RegisterPage } from 'components';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getCurrentUser } from 'redux/auth/authOperations';
+import { selectIsAuth } from 'redux/auth/authSelectop';
+
 export const App = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >d
-      React homework template
-    </div>
+    <>
+      {isAuth ? (
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/contacts" element={<ContactPage />} />
+            <Route path="*" element={<Navigate to="/contacts" />} />
+          </Route>
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 };
